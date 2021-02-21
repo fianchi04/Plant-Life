4216c1f095b17c779f0663937532ec56469e580f
Anna <av.wilde04+github@gmail.com>
2021-02-21 22:01:40 +0000

    initial commit

---
+++ app/src/main/java/com/fianchi/plantlife/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.fianchi.plantlife
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+
+
+const val NAME_NEW_PLANT = "com.fianchi.myfirstapp.MESSAGE"
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun saveNewPlant(view: View) {
+        val savePlant = findViewById<EditText>(R.id.editTextTextPersonName)
+        val message = savePlant.text.toString()
+        val intent = Intent(this, SaveNewPlantActivity::class.java).apply {
+            putExtra(NAME_NEW_PLANT, message)
+        }
+        startActivity(intent)
+
+    }
+}
