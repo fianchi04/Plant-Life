8b5223498da136d34e609f6b9d76ef3991f37d01
Anna <av.wilde04+github@gmail.com>
2021-02-22 21:23:27 +0000

    Added schema for Plant table, using SQLite. App takes input on the home screen to enter a name for a plant, then saves a record to the db and returns a list of all the records in the table to the user on a second page

--- app/src/main/java/com/fianchi/plantlife/MainActivity.kt
+++ app/src/main/java/com/fianchi/plantlife/MainActivity.kt
@@ -24,4 +24,8 @@ class MainActivity : AppCompatActivity() {
         startActivity(intent)
 
     }
+
+    fun savePlantToDb(view: View) {
+
+    }
 }
--- app/src/main/java/com/fianchi/plantlife/SaveNewPlantActivity.kt
+++ app/src/main/java/com/fianchi/plantlife/SaveNewPlantActivity.kt
@@ -3,6 +3,7 @@ package com.fianchi.plantlife
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import java.util.*
 
 class SaveNewPlantActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,9 +13,20 @@ class SaveNewPlantActivity : AppCompatActivity() {
         // Get the Intent that started this activity and extract the string
         val message = intent.getStringExtra(NAME_NEW_PLANT)
 
+        val plantDatabaseHandler: PlantDatabaseHandler = PlantDatabaseHandler(this)
+        val status = plantDatabaseHandler.addPlant(Plant(id = UUID.randomUUID().toString(), message!!, "UNKNOWN SPECIES"))
+
         // Capture the layout's TextView and set the string as its text
         val textView = findViewById<TextView>(R.id.textView).apply {
-            text = message
+            text =
+                    if (status.equals(-1)) "Failed to save $message to the db"
+                    else {
+                        """Saved $message to DB
+                    All DB entries:
+                    ${plantDatabaseHandler.viewPlant().toString()}
+                """.trimMargin()
+                    }
         }
     }
+
 }
